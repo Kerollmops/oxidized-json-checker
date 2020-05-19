7ac7fb836cea429cf8558ec638eedbfbb09f1ef7
Kerollmops <clement@meilisearch.com>
2020-05-19 15:47:10 +0200

    Introduce the ojc binary

--- src/bin/ojc.rs
+++ src/bin/ojc.rs
@@ -1,11 +1,18 @@
 use std::io;
 use oxidized_json_checker::JsonChecker;
 
-fn main() -> io::Result<()> {
+fn fmain() -> io::Result<()> {
     let stdin = io::stdin();
     let mut checker = JsonChecker::new(stdin.lock());
     io::copy(&mut checker, &mut io::sink())?;
     let outer_type = checker.finish()?;
-    eprintln!("{:?}", outer_type);
+    println!("{:?}", outer_type);
     Ok(())
+}
+
+fn main() {
+    if let Err(e) = fmain() {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
 }
